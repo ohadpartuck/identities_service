8bd920c9895a8d29f04dbe3568a0c5c34727f108
ohad partuck <ohadpartuck@gmail.com>
2014-04-20 08:13:17 +0300

    working on pulling users data from facebook

--- routes/identity.js
+++ routes/identity.js
@@ -2,10 +2,6 @@ var identity    = require(GLOBAL.ROOT + '/lib/identities/identities.js');
 
 module.exports = function (app) {
     app.namespace('/identity/:provider_name', function(){
-        app.get('/', function (req, res, _) {
-            res.json({iasdfasdfasdf: 'index'});
-        });
-
         app.post('/new', function (req, res, _) {
             res.json(identity.create_a_new_identity(req));
         });
