49debd9866e2ea1760eb12ec0f1092d07c5a1910
ohad partuck <ohadpartuck@gmail.com>
2014-05-05 17:34:47 +0300

    adding some config

--- init/constants.js
+++
@@ -1,7 +0,0 @@
-
-GLOBAL.DEFAULT_PROVIDER         = 'facebook';
-
-GLOBAL.FACEBOOK_GRAPH_URL       = 'https://graph.facebook.com/';
-
-GLOBAL.CLIENT_ID                = '727304957314097';
-GLOBAL.CLIENT_SECRET            = '9477e749ebc28bca7af4272d6be19cb1';
