1def5632aaa345f0d080f05e3548d759e090cfc8
ohad partuck <ohadpartuck@gmail.com>
2014-05-10 17:23:38 +0300

    trying to sign up with facebook

--- routes/sanger/social_api.js
+++ routes/sanger/social_api.js
@@ -2,9 +2,9 @@ var passport        = require('passport');
 
 module.exports = function (router, namespace) {
 
-    router.get(namespace + '/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-    router.get(namespace + '/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
-        res.json({})
+//    router.get(namespace + '/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+    router.post(namespace + '/auth/:providerName/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
+        res.json({auth: req.params.providerName})
     });
 
     return router;
